70eef89b0dbf3afef12a77ec18e951666a0257f3
Helen Lehrer <hlehrer@outlook.com>
2023-06-15 11:25:55 -0700

    Add '#2: What is the difference between var and let keywords in javascript?'

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function hoistedFunction(){
 
 //ex#3
 //takes place in the local scope as well
-function doSomething(){
+function localScope(){
   x=33;
   console.log(x);
   var x;
@@ -25,5 +25,27 @@ function doSomething(){
 var x;
 console.log(x);
 x=23;
+//undefined
+
+
+console.log(y);
+var y=23;
+//undefined
+
 //to avoid hoisting, you can run javascript in strict mode by using "use strict" on top of the code
 
+//#2: What is the difference between var and let keywords in javascript?
+//var has used since the very beginning of Javascript, whereas 'let' was introduced in 2015.
+//Var has a function scope. 'Let' scope is limited to the block in which it was declared. 
+//let and const are hoisted but not initialized.Referencing the variable in the block before the variable declaration results in a ReferenceError because the variable is in a "temporal dead zone" from the start of the block until the declaration is processed.
+
+//ex#1 Temporal Dead Zone
+// console.log(let1);
+// let let1 = 5;
+//Reference Error
+
+//ex#2
+let let2;
+console.log(let2)
+let2 = 5;
+//undefined
